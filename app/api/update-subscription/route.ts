2e92efb6899a63f9a229bea9e96c51ef208c905d
kaushikdut <duttakoushik779@gmail.com>
2025-09-18 19:59:23 +0530

    fix: authentication

--- app/api/update-subscription/route.ts
+++ app/api/update-subscription/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/prisma";
 
 export async function POST(req: Request) {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     if (!session) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
--- app/api/user-subscription/route.ts
+++ app/api/user-subscription/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     if (!session) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
